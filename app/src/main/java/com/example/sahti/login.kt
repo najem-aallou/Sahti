e8f93a9b481b1d312c170c34bcf98253e57fe013
najem-aallou <aallou.najem1@gmail.com>
2024-11-24 18:48:28 +0100

    Version 2

--- app/src/main/java/com/example/sahti/login.kt
+++ app/src/main/java/com/example/sahti/login.kt
@@ -1,5 +1,6 @@
 package com.example.sahti
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
@@ -8,25 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Page4 : AppCompatActivity() {
+class login : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_page4)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val bt_back1 = findViewById<TextView>(R.id.bt_back1)
-        bt_back1.setOnClickListener {
-            val intent = Intent(this, Page3::class.java)
-            startActivity(intent)
-        }
-        val bt3 = findViewById<TextView>(R.id.bt3)
-       bt3.setOnClickListener {
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
         }
     }
 }
